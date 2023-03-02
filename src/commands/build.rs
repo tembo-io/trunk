b75f04b2d6692eb3fe828a26f1a110991ff2a81c
Steven Miller <sjmiller609@gmail.com>
2023-03-02 14:18:06 -0500

    WIP: build

--- src/commands/build.rs
+++ src/commands/build.rs
@@ -17,10 +17,7 @@ impl SubCommand for BuildCommand {
     fn execute(&self) {
         println!("Building from path {}", self.path);
         let path = Path::new(&self.path);
-        // check if Cargo.toml is present in the path self.path
-
         if path.join("Cargo.toml").exists() {
-            // parse Cargo.toml
             let cargo_toml: Table = toml::from_str(&std::fs::read_to_string(path.join("Cargo.toml")).unwrap()).unwrap();
             let dependencies = cargo_toml.get("dependencies").unwrap().as_table().unwrap();
             if dependencies.contains_key("pgx") {
--- src/commands/pgx.rs
+++ src/commands/pgx.rs
@@ -1,6 +1,11 @@
 use std::path::Path;
+use std::include_str;
+
 
 pub fn build_pgx(path: &Path, output_path: &str) {
     // your code for building a pgx extension goes here
     println!("Building pgx extension at path {}", &path.display());
+    let dockerfile = include_str!("./pgx_builder/Dockerfile");
+    println!("{}", dockerfile);
+
 }
