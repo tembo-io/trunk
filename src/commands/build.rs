fe39b89620269e12aaa9583c05c9d1b635e4ac78
Steven Miller <sjmiller609@gmail.com>
2023-03-02 12:03:29 -0500

    Add CLion config

--- src/commands/build.rs
+++ src/commands/build.rs
@@ -2,10 +2,14 @@ use super::SubCommand;
 use clap::Args;
 
 #[derive(Args)]
-pub struct BuildCommand {}
+pub struct BuildCommand {
+    #[arg(long = "path", default_value = ".")]
+    path: String,
+}
 
 impl SubCommand for BuildCommand {
     fn execute(&self) {
+        println!("{}", self.path);
         println!("trunk build: not implemented")
     }
 }
