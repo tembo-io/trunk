749509e60d61b5efc064b2ccefce792f12e5a9f0
Karl Morand <karljmorand@gmail.com>
2023-05-10 14:44:01 -0400

    add favicon

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { ClerkProvider, ClerkLoaded } from "@clerk/nextjs";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -5,13 +6,18 @@ function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
   return (
-    <ClerkProvider {...pageProps}>
-      <ClerkLoaded>
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </ClerkLoaded>
-    </ClerkProvider>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <ClerkProvider {...pageProps}>
+        <ClerkLoaded>
+          <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+          </QueryClientProvider>
+        </ClerkLoaded>
+      </ClerkProvider>
+    </>
   );
 }
 
