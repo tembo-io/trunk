44db49952cbf2f05e38e4213668a51fc45641fb2
Karl Morand <karljmorand@gmail.com>
2023-05-10 19:33:01 -0400

    search box and button

--- ui/pages/search/index.tsx
+++ ui/pages/search/index.tsx
@@ -1,3 +1,58 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import styles from "./search.module.scss";
+import Link from "next/link";
+import cx from "classnames";
+
+import { Inter } from "next/font/google";
+import { useUser, useAuth } from "@clerk/nextjs";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import Header from "@/components/Header";
+import { ExtensionListing } from "@/types";
+import fetchExtensions from "@/lib/fetchExtensions";
+import { formatDistanceToNow } from "date-fns";
+import LoadingSpinner from "@/components/LoadingSpinner";
+const inter = Inter({ subsets: ["latin"], weight: ["400", "700"] });
+
 export default function SearchPage() {
-  return <div>SEARCH PAGE</div>;
+  const router = useRouter();
+  const { q: query } = router.query;
+  const [searchQuery, setSearchQuery] = useState<string>(query?.toString() ?? "");
+  const { isLoading, data } = useQuery<ExtensionListing[]>(["extList"], fetchExtensions);
+  console.log("query", query);
+
+  const handleChange = (input: string) => {
+    setSearchQuery(input);
+    router.push(`/search?q=${input}`);
+  };
+
+  const filteredItems =
+    data?.filter(
+      (item) =>
+        item?.name.toLowerCase().includes(searchQuery?.toLowerCase()) ||
+        item?.description?.toLowerCase().includes(searchQuery?.toLowerCase())
+    ) ?? [];
+
+  return (
+    <div>
+      <Header></Header>
+      <div className={styles.searchRow}>
+        <input
+          className={cx(inter.className, styles.input)}
+          type="text"
+          placeholder="Search extensions"
+          value={searchQuery}
+          onChange={(e) => handleChange(e.target.value.toString())}
+        />
+        <button className={cx(inter.className, styles.searchButton)}>Search</button>
+      </div>
+      <div>
+        <div>
+          {/* {filteredItems.map((item) => (
+
+          ))} */}
+        </div>
+      </div>
+    </div>
+  );
 }
