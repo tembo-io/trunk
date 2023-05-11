04805fd84a0fb433bf37319a7871b3455e9a21cd
Karl Morand <karljmorand@gmail.com>
2023-05-10 22:30:13 -0400

    Cor 755 extension search page (#68)

--- ui/components/Hero/index.tsx
+++ ui/components/Hero/index.tsx
@@ -1,5 +1,6 @@
 import cx from "classnames";
 import styles from "./Hero.module.scss";
+import Image from "next/image";
 import ExtensionSearchBox from "../ExtensionSearchBox";
 import { Inter } from "next/font/google";
 
@@ -13,6 +14,10 @@ export default function Hero() {
         <ExtensionSearchBox></ExtensionSearchBox>
       </div>
       <h2 className={cx(inter.className, styles.subtitle)}>The easiest way to publish and install PostgreSQL extensions. </h2>
+      <a href="https://github.com/CoreDB-io/trunk" className={styles.repoButton}>
+        <Image src="/images/github.svg" alt="GitHub logo" width={20} height={20}></Image>
+        <span className={cx(inter.className, styles.repoText)}>View on GitHub</span>
+      </a>
       <p className={cx(inter.className, styles.body)}>
         Trunk is an open-source package installer and registry for PostgreSQL extensions. Use the Trunk CLI to easily publish and install
         PostgreSQL extensions and their dependencies.
--- ui/pages/search/index.tsx
+++ ui/pages/search/index.tsx
@@ -1,3 +1,85 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import styles from "./search.module.scss";
+import Link from "next/link";
+import cx from "classnames";
+import { truncateString } from "@/lib/truncateString";
+
+import { Inter } from "next/font/google";
+import { useQuery } from "@tanstack/react-query";
+import Header from "@/components/Header";
+import { ExtensionListing } from "@/types";
+import fetchExtensions from "@/lib/fetchExtensions";
+import { formatDistanceToNow } from "date-fns";
+import LoadingSpinner from "@/components/LoadingSpinner";
+const inter = Inter({ subsets: ["latin"], weight: ["400", "700"] });
+
 export default function SearchPage() {
-  return <div>SEARCH PAGE</div>;
+  const router = useRouter();
+  const { q: query } = router.query;
+  const [searchQuery, setSearchQuery] = useState<string>(query?.toString() ?? "");
+  const { isLoading, data } = useQuery<ExtensionListing[]>(["extList"], fetchExtensions);
+  console.log("query", query);
+
+  const handleChange = (input: string) => {
+    setSearchQuery(input);
+    router.push(`/search?q=${input}`);
+  };
+  const sortedExtensions = data?.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    return 1;
+  });
+
+  const filteredItems =
+    sortedExtensions?.filter(
+      (item) =>
+        item?.name.toLowerCase().includes(searchQuery?.toLowerCase()) ||
+        item?.description?.toLowerCase().includes(searchQuery?.toLowerCase())
+    ) ?? [];
+
+  return (
+    <div>
+      <Header></Header>
+      <div className={styles.searchRow}>
+        <input
+          className={cx(inter.className, styles.input)}
+          type="text"
+          placeholder="Search extensions"
+          value={searchQuery}
+          onChange={(e) => handleChange(e.target.value.toString())}
+        />
+        <button className={cx(inter.className, styles.searchButton)}>Search</button>
+      </div>
+      {isLoading && <LoadingSpinner size="lg"></LoadingSpinner>}
+      <div>
+        <div className={styles.extensionList}>
+          {filteredItems.map((ext) => {
+            let extDate = "";
+            if (ext?.updatedAt) {
+              extDate = ext?.updatedAt.split(" +")[0];
+            }
+            return (
+              <Link href={`/extensions/${ext.name}`} key={ext.name} className={styles.extCont}>
+                <div className={styles.titleRow}>
+                  <p className={styles.extName}>{ext.name}</p>
+                  <p className={styles.extVersion}>v{ext.latestVersion}</p>
+                </div>
+                <p className={styles.extDesc}>{truncateString(ext.description, 200)}</p>
+                <div className={styles.extFooter}>
+                  <p className={styles.extAuthor}>
+                    {ext.owners.map((owner) => (
+                      <span key={owner.userId}>{owner.userName}</span>
+                    ))}
+                  </p>
+                  <p className={styles.extLastUpdated}>{extDate ? `${formatDistanceToNow(new Date(extDate))} ago` : ""}</p>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
