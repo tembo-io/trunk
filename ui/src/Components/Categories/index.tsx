b202ee91533828b8e5415a2c7a56106ae1bc87fe
Karl Morand <karljmorand@gmail.com>
2023-06-22 15:35:06 -0400

    indicate active category

--- ui/src/Components/Categories/index.tsx
+++ ui/src/Components/Categories/index.tsx
@@ -3,8 +3,11 @@ import Link from "next/link";
 import styles from "./categories.module.scss";
 import cx from "classnames";
 import { Category } from "@/types";
+import { useRouter } from "next/router";
 
 export default function Categories({ categories }: { categories: Category[] }) {
+  const router = useRouter();
+  const { cat } = router.query;
   return (
     <div className={styles.container}>
       {categories
@@ -14,7 +17,9 @@ export default function Categories({ categories }: { categories: Category[] }) {
             <Link shallow href={`/?cat=${item.slug}`} className={styles.interMed16}>
               {item.name}
             </Link>
-            <span className={cx(styles.catCount, styles.interReg12)}>{item.extension_count}</span>
+            <span className={cx(styles.catCount, styles.interReg12, cat === item.slug ? styles.activeCat : "")}>
+              {item.extension_count}
+            </span>
           </div>
         ))}
     </div>
