cba7565064cf9c48426b19035397fe849d756569
Karl Morand <karljmorand@gmail.com>
2023-06-26 12:59:29 -0400

    implement copy button

--- ui/src/Components/Hero/index.tsx
+++ ui/src/Components/Hero/index.tsx
@@ -1,17 +1,42 @@
+import { useState, useEffect } from "react";
+import cx from "classnames";
 import Image from "next/image";
 import styles from "./hero.module.scss";
 const BgImage = "/bg_text.png";
-const TopSwoop = "/top_swoop.png";
-const BottomSwoop = "/bottom_swoop.png";
+
 export default function Hero() {
+  const [showFeedback, setShowFeedback] = useState(false);
+
+  useEffect(() => {
+    if (showFeedback) {
+      const timer = setTimeout(() => {
+        setShowFeedback(false);
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [showFeedback]);
+
+  const handleCopy = async () => {
+    try {
+      navigator.clipboard.writeText("cargo install pg-trunk");
+      setShowFeedback(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.installCont}>
         <p className={styles.getTrunk}>Get Trunk</p>
         <div className={styles.installRow}>
+          <div className={cx(styles.buttonFeedback, showFeedback ? styles.showButtonFeedback : "")}>Copied to clipboard!</div>
           <div className={styles.installDropdown}>Cargo</div>
           <p className={styles.installCode}>cargo install pg-trunk</p>
-          <button className={styles.copyButton}>Copy</button>
+          <button onClick={handleCopy} className={styles.copyButton}>
+            Copy
+          </button>
         </div>
         <p className={styles.installDesc}>Trunk is an open-source package installer and registry for PostgreSQL extensions.</p>
       </div>
