fcff921cc2f39a015a542746caae7eecdf15968d
Steven Miller <sjmiller609@gmail.com>
2023-07-02 15:13:54 -0400

    Add test

--- cli/tests/integration_tests.rs
+++ cli/tests/integration_tests.rs
@@ -291,3 +291,28 @@ fn build_pg_stat_statements() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+#[test]
+fn build_pg_cron_trunk_toml() -> Result<(), Box<dyn std::error::Error>> {
+    let mut rng = rand::thread_rng();
+    let output_dir = format!("/tmp/pg_cron_test_{}", rng.gen_range(0..1000000));
+
+    // Construct a path relative to the current file's directory
+    let mut trunkfile_path = std::path::PathBuf::from(file!());
+    trunkfile_path.pop(); // Remove the file name from the path
+    trunkfile_path.push("test_trunk_toml_dirs");
+    trunkfile_path.push("pg_cron");
+
+    let mut cmd = Command::cargo_bin(CARGO_BIN)?;
+    cmd.arg("build");
+    cmd.arg("--path");
+    cmd.arg(trunkfile_path.as_os_str());
+    cmd.arg("--output-path");
+    cmd.arg(output_dir.clone());
+    cmd.assert().code(0);
+    assert!(std::path::Path::new(format!("{output_dir}/pg_cron-1.5.2.tar.gz").as_str()).exists());
+    // delete the temporary file
+    std::fs::remove_dir_all(output_dir)?;
+
+    Ok(())
+}
