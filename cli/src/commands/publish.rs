19f0564c907e09541ff803c71a3fe15edf68a661
Ian Stanton <ian@coredb.io>
2023-07-06 14:20:27 -0400

    Add `Trunk.toml` support to `trunk publish` (#153)

--- cli/src/commands/publish.rs
+++ cli/src/commands/publish.rs
@@ -1,7 +1,11 @@
 use super::SubCommand;
 use crate::commands::categories::VALID_CATEGORY_SLUGS;
 use crate::commands::publish::PublishError::InvalidExtensionName;
-use anyhow::anyhow;
+use crate::config;
+use crate::config::{
+    get_from_trunk_toml_if_not_set_on_cli, get_string_vec_from_trunk_toml_if_not_set_on_cli,
+};
+use anyhow::{anyhow, Context};
 use async_trait::async_trait;
 use clap::Args;
 use reqwest::header::CONTENT_TYPE;
@@ -9,15 +13,16 @@ use reqwest::header::{HeaderMap, AUTHORIZATION};
 use reqwest::StatusCode;
 use serde::Deserialize;
 use serde_json::json;
-use std::path::PathBuf;
+use std::fs::File;
+use std::path::{Path, PathBuf};
 use std::{env, fs};
 use tokio_task_manager::Task;
 
 #[derive(Args)]
 pub struct PublishCommand {
-    name: String,
+    name: Option<String>,
     #[arg(long = "version", short = 'v')]
-    version: String,
+    version: Option<String>,
     #[arg(long = "file", short = 'f')]
     file: Option<PathBuf>,
     #[arg(long = "description", short = 'd')]
@@ -28,12 +33,8 @@ pub struct PublishCommand {
     homepage: Option<String>,
     #[arg(long = "license", short = 'l')]
     license: Option<String>,
-    #[arg(
-        long = "registry",
-        short = 'r',
-        default_value = "https://registry.pgtrunk.io"
-    )]
-    registry: String,
+    #[arg(long = "registry", short = 'r')]
+    registry: Option<String>,
     #[arg(long = "repository", short = 'R')]
     repository: Option<String>,
     #[arg(long = "category", short = 'c')]
@@ -53,16 +54,178 @@ pub struct Category {
     pub slug: String,
 }
 
+pub struct PublishSettings {
+    name: String,
+    version: String,
+    file: Option<PathBuf>,
+    description: Option<String>,
+    documentation: Option<String>,
+    homepage: Option<String>,
+    license: Option<String>,
+    registry: String,
+    repository: Option<String>,
+    categories: Option<Vec<String>>,
+}
+
+impl PublishCommand {
+    fn settings(&self) -> Result<PublishSettings, anyhow::Error> {
+        let path = ".".to_string();
+        let trunkfile_path = Path::new(&path.clone()).join("Trunk.toml");
+        let trunk_toml = match File::open(trunkfile_path) {
+            Ok(file) => config::parse_trunk_toml(file),
+            Err(_e) => {
+                println!("Trunk.toml not found");
+                Ok(None)
+            }
+        }?;
+
+        let name = match self.name.clone() {
+            Some(name) => name,
+            None => match get_from_trunk_toml_if_not_set_on_cli(
+                None,
+                trunk_toml.clone(),
+                "extension",
+                "name",
+            ) {
+                Some(trunk_toml_name) => trunk_toml_name,
+                None => panic!(
+                    "Extension name must be provided when publishing. Please specify the extension name \
+                     as the first argument, or under extension.name in Trunk.toml"
+                ),
+            },
+        };
+
+        let version = match self.version.clone() {
+            Some(version) => version,
+            None => match get_from_trunk_toml_if_not_set_on_cli(
+                None,
+                trunk_toml.clone(),
+                "extension",
+                "version",
+            ) {
+                Some(trunk_toml_version) => trunk_toml_version,
+                None => panic!(
+                    "Extension version must be provided when publishing. Please specify the extension version \
+                     with --version, or under extension.version in Trunk.toml"
+                ),
+            },
+        };
+
+        // file
+        let file = match self.file.clone() {
+            Some(file) => Some(file),
+            None => match trunk_toml.clone() {
+                Some(table) => match table.get("extension") {
+                    Some(extension) => match extension.get("file") {
+                        Some(value) => {
+                            let result = value.as_str().unwrap_or_else(|| {
+                                panic!("Trunk.toml: extension.file should be a string")
+                            });
+                            let mut path = PathBuf::new();
+                            let _ = &path.push(result);
+                            println!(
+                                "Trunk.toml: using setting extension.file: {}",
+                                path.to_str().unwrap()
+                            );
+                            Some(path)
+                        }
+                        None => None,
+                    },
+                    None => None,
+                },
+                None => None,
+            },
+        };
+
+        // description
+        let description = get_from_trunk_toml_if_not_set_on_cli(
+            self.description.clone(),
+            trunk_toml.clone(),
+            "extension",
+            "description",
+        );
+
+        // documentation
+        let documentation = get_from_trunk_toml_if_not_set_on_cli(
+            self.documentation.clone(),
+            trunk_toml.clone(),
+            "extension",
+            "documentation",
+        );
+
+        // homepage
+        let homepage = get_from_trunk_toml_if_not_set_on_cli(
+            self.homepage.clone(),
+            trunk_toml.clone(),
+            "extension",
+            "homepage",
+        );
+
+        // license
+        let license = get_from_trunk_toml_if_not_set_on_cli(
+            self.license.clone(),
+            trunk_toml.clone(),
+            "extension",
+            "license",
+        );
+
+        // registry
+        let registry = match self.registry.clone() {
+            Some(registry) => registry,
+            None => match get_from_trunk_toml_if_not_set_on_cli(
+                None,
+                trunk_toml.clone(),
+                "extension",
+                "registry",
+            ) {
+                Some(trunk_toml_registry) => trunk_toml_registry,
+                None => "https://registry.pgtrunk.io".to_string(),
+            },
+        };
+
+        // repository
+        let repository = get_from_trunk_toml_if_not_set_on_cli(
+            self.repository.clone(),
+            trunk_toml.clone(),
+            "extension",
+            "repository",
+        );
+
+        // categories
+        let categories = get_string_vec_from_trunk_toml_if_not_set_on_cli(
+            self.category.clone(),
+            trunk_toml.clone(),
+            "extension",
+            "categories",
+        );
+
+        Ok(PublishSettings {
+            version,
+            file,
+            description,
+            documentation,
+            homepage,
+            license,
+            registry,
+            repository,
+            name,
+            categories,
+        })
+    }
+}
+
 #[async_trait]
 impl SubCommand for PublishCommand {
     async fn execute(&self, _task: Task) -> Result<(), anyhow::Error> {
+        let publish_settings = self.settings()?;
         // Validate extension name input
         let mut slugs = Vec::new();
 
-        check_input(&self.name)?;
+        check_input(&publish_settings.name)?;
         // Validate categories input if provided
-        if self.category.is_some() {
-            let response = reqwest::get(&format!("{}/categories/all", self.registry)).await?;
+        if publish_settings.categories.is_some() {
+            let response =
+                reqwest::get(&format!("{}/categories/all", publish_settings.registry)).await?;
             match response.status() {
                 StatusCode::OK => {
                     let response_body = response.text().await?;
@@ -74,7 +237,7 @@ impl SubCommand for PublishCommand {
                 }
                 _ => {
                     // Fall back to local file if we fail to fetch valid slugs from registry
-                    println!("Error fetching valid category slugs from {}/categories/all. Falling back to local definitions in categories.rs", self.registry);
+                    println!("Error fetching valid category slugs from {}/categories/all. Falling back to local definitions in categories.rs", publish_settings.registry);
                     slugs = VALID_CATEGORY_SLUGS
                         .to_vec()
                         .into_iter()
@@ -83,30 +246,35 @@ impl SubCommand for PublishCommand {
                 }
             }
 
-            let categories = self.category.clone().unwrap();
+            let categories = publish_settings.categories.clone().unwrap();
             for category in categories {
                 if !slugs.contains(&category) {
-                    return Err(anyhow!("Invalid category slug: {}. \nValid category slugs: {:?} \nMore details can be found at {}/categories/all", category, slugs, self.registry));
+                    return Err(anyhow!("Invalid category slug: {}. \nValid category slugs: {:?} \nMore details can be found at {}/categories/all", category, slugs, publish_settings.registry));
                 }
             }
         }
 
-        let (file, name) = match &self.file {
+        let (file, name) = match &publish_settings.file {
             Some(..) => {
                 // If file is specified, use it
-                let path = self.file.clone().unwrap();
+                let path = publish_settings.file.clone().unwrap();
                 let name = path.file_name().unwrap().to_str().unwrap().to_owned();
-                let f = fs::read(self.file.clone().unwrap())?;
+                let f = fs::read(publish_settings.file.clone().unwrap())
+                    .context(format!("Could not find file '{}'", path.to_str().unwrap()))?;
                 (f, name)
             }
             None => {
                 // If no file is specified, read file from working dir with format
-                // <extension_name>-<version>.tar.gz.
+                // .trunk/<extension_name>-<version>.tar.gz.
                 // Error if file is not found
                 let mut path = PathBuf::new();
-                let _ = &path.push(format!("./{}-{}.tar.gz", self.name, self.version));
+                let _ = &path.push(format!(
+                    ".trunk/{}-{}.tar.gz",
+                    publish_settings.name, publish_settings.version
+                ));
                 let name = path.file_name().unwrap().to_str().unwrap().to_owned();
-                let f = fs::read(path.clone())?;
+                let f = fs::read(path.clone())
+                    .context(format!("Could not find file '{}'", path.to_str().unwrap()))?;
                 (f, name)
             }
         };
@@ -119,21 +287,21 @@ impl SubCommand for PublishCommand {
             .file_name(name)
             .headers(headers.clone());
         let m = json!({
-            "name": self.name,
-            "vers": self.version,
-            "description": self.description,
-            "documentation": self.documentation,
-            "homepage": self.homepage,
-            "license": self.license,
-            "repository": self.repository,
-            "categories": self.category
+            "name": publish_settings.name,
+            "vers": publish_settings.version,
+            "description": publish_settings.description,
+            "documentation": publish_settings.documentation,
+            "homepage": publish_settings.homepage,
+            "license": publish_settings.license,
+            "repository": publish_settings.repository,
+            "categories": publish_settings.categories
         });
         let metadata = reqwest::multipart::Part::text(m.to_string()).headers(headers);
         let form = reqwest::multipart::Form::new()
             .part("metadata", metadata)
             .part("file", file_part);
         let client = reqwest::Client::new();
-        let url = format!("{}/extensions/new", self.registry);
+        let url = format!("{}/extensions/new", publish_settings.registry);
         let res = client
             .post(url)
             .multipart(form)
@@ -147,7 +315,7 @@ impl SubCommand for PublishCommand {
     }
 }
 
-pub fn check_input(input: &str) -> Result<(), PublishError> {
+pub fn check_input(input: &String) -> Result<(), PublishError> {
     let valid = input
         .as_bytes()
         .iter()
--- cli/src/config.rs
+++ cli/src/config.rs
@@ -20,7 +20,7 @@ pub fn get_from_trunk_toml_if_not_set_on_cli(
                             })
                             .to_string();
                         println!(
-                            "Trunk.toml: using build setting {}.{}: {}",
+                            "Trunk.toml: using setting {}.{}: {}",
                             table_name, key, result
                         );
                         Some(result)
@@ -29,6 +29,42 @@ pub fn get_from_trunk_toml_if_not_set_on_cli(
                         println!("Trunk.toml: {}.{} is not set", table_name, key);
                         None
                     }
+                },
+                None => None,
+            },
+            None => None,
+        },
+    }
+}
+
+pub fn get_string_vec_from_trunk_toml_if_not_set_on_cli(
+    cli_setting: Option<Vec<String>>,
+    trunk_toml: Option<Table>,
+    table_name: &str,
+    key: &str,
+) -> Option<Vec<String>> {
+    match cli_setting {
+        Some(cli_setting) => Some(cli_setting),
+        None => match trunk_toml.clone() {
+            Some(table) => match table.get(table_name) {
+                Some(extension) => match extension.get(key) {
+                    Some(value) => {
+                        let result = value.as_array().unwrap_or_else(|| {
+                            panic!(
+                                "Trunk.toml: {}.{} should be an array of strings",
+                                table_name, key
+                            )
+                        });
+                        let mut v: Vec<String> = Vec::new();
+                        for i in result {
+                            let s = i.as_str();
+                            let s = s.unwrap().to_string();
+                            v.push(s);
+                        }
+                        println!("Trunk.toml: using setting {}.{}: {:?}", table_name, key, v);
+                        Some(v)
+                    }
+                    None => None,
                 },
                 None => None,
             },
@@ -51,7 +87,6 @@ pub fn parse_trunk_toml<R: Read>(mut reader: R) -> Result<Option<Table>, anyhow:
 
 #[cfg(test)]
 mod tests {
-
     use crate::config::parse_trunk_toml;
 
     #[test]
@@ -60,6 +95,14 @@ mod tests {
         [extension]
         name = "pg_cron"
         version = "1.5.2"
+        repository = "https://github.com/citusdata/pg_cron"
+        license = "PostgreSQL"
+        description = "Run periodic jobs in PostgreSQL"
+        homepage = "https://github.com/citusdata/pg_cron"
+        documentation = "https://github.com/citusdata/pg_cron"
+        categories = ["analytics", "debugging"]
+        registry = "https://my.dummy.registry.dev"
+
 
         [build]
         dockerfile = "Dockerfile"
@@ -69,6 +112,39 @@ mod tests {
         let table = result.expect("Expected a table");
         assert_eq!(table["extension"]["name"].as_str().unwrap(), "pg_cron");
         assert_eq!(table["extension"]["version"].as_str().unwrap(), "1.5.2");
+        assert_eq!(
+            table["extension"]["repository"].as_str().unwrap(),
+            "https://github.com/citusdata/pg_cron"
+        );
+        assert_eq!(
+            table["extension"]["license"].as_str().unwrap(),
+            "PostgreSQL"
+        );
+        assert_eq!(
+            table["extension"]["description"].as_str().unwrap(),
+            "Run periodic jobs in PostgreSQL"
+        );
+        assert_eq!(
+            table["extension"]["homepage"].as_str().unwrap(),
+            "https://github.com/citusdata/pg_cron"
+        );
+        assert_eq!(
+            table["extension"]["documentation"].as_str().unwrap(),
+            "https://github.com/citusdata/pg_cron"
+        );
+        let categories = table["extension"]["categories"].as_array().unwrap();
+        let mut v: Vec<String> = Vec::new();
+        for i in categories {
+            let s = i.as_str();
+            let s = s.unwrap().to_string();
+            v.push(s);
+        }
+        assert_eq!(v, vec!("analytics", "debugging"));
+        assert_eq!(
+            table["extension"]["registry"].as_str().unwrap(),
+            "https://my.dummy.registry.dev"
+        );
+
         assert_eq!(table["build"]["dockerfile"].as_str().unwrap(), "Dockerfile");
         assert_eq!(
             table["build"]["install_command"].as_str().unwrap(),
