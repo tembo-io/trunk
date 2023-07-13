a00f0629f573b5f08d6b617b63cab1871c52dc61
Karl Morand <karljmorand@gmail.com>
2023-07-13 13:52:37 -0400

    remove token check

--- ui/src/api/revalidate/[ext].ts
+++ ui/src/api/revalidate/[ext].ts
@@ -1,10 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
-
   try {
     const { ext } = req.query;
     await res.revalidate(`/extensions/${ext}`);
