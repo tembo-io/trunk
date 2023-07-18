fdffc97017b75c792088806f158c8195975b8dc8
Karl Morand <karljmorand@gmail.com>
2023-07-18 15:40:33 -0400

    update footer

--- ui/src/Components/Footer/index.tsx
+++ ui/src/Components/Footer/index.tsx
@@ -1,5 +1,14 @@
 import styles from "./footer.module.scss";
+import Image from "next/image";
+import TrunkLogo from "/public/TemboElephant.png";
 
 export default function Footer() {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <footer className={styles.footer}>
+      <a className={styles.link} href="https://tembo.io" target="_blank">
+        <span className={styles.small}> Sponsored by</span>
+        <Image className={styles.image} src={TrunkLogo} width={20} height={20} alt="Tembo logo"></Image>Tembo
+      </a>
+    </footer>
+  );
 }
