c2432f31a8ab32e1ec05f98ec1c2c6ea546980d1
Karl Morand <karljmorand@gmail.com>
2023-07-18 15:08:17 -0400

    add footer (#227)

---
+++ ui/src/Components/Footer/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./footer.module.scss";
+
+export default function Footer() {
+  return <footer className={styles.footer}></footer>;
+}
--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -3,9 +3,9 @@ import "@/globals.scss";
 import type { AppProps } from "next/app";
 import { Inter, IBM_Plex_Mono } from "next/font/google";
 import "../styles/markdown.css";
+import Footer from "@/Components/Footer";
 const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "600", "700"], variable: "--font-inter" });
 export const plex_mono = IBM_Plex_Mono({ weight: ["700", "400"], subsets: ["latin"], variable: "--font-plex" });
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <main className={`${plex_mono.variable} ${inter.variable}`}>
         <Component {...pageProps} />
+        <Footer />
       </main>
     </>
   );
