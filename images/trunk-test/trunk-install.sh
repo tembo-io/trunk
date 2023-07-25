5ac568ade713aff47a6e0b5df72ef669988dd95b
Ian Stanton <ian@coredb.io>
2023-07-25 06:37:37 -0500

    Print failed extensions

--- images/trunk-test/trunk-install.sh
+++ images/trunk-test/trunk-install.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-failures=0
+failure_count=0
+failed_extensions=()
 file='/tmp/extensions.txt'
 lines=$(cat $file)
 for line in $lines
 do
         trunk install $line
         psql -c "create extension \"$line\" cascade;"
-        if [ $? -eq 0 ]; then
-            echo "Command succeeded"
-        else
-            echo "Command failed"
-            let "failures++"
+        if [ $? -ne 0 ]; then
+            echo "CREATE EXTENSION command failed"
+            let "failure_count++"
+            failed_extensions+=("$line")
         fi
         printf "\n\n"
 done
-echo "FAILURES: $failures"
+echo "*** FAILURE COUNT ***: $failure_count"
+printf "\n*** FAILED EXTENSIONS ***:\n"
+for failed in "${failed_extensions[@]}"
+do
+      echo $failed
+done
