429f0ce1ff2299f61a892ea5970ff3c8b272ddc9
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2023-08-03 00:07:06 -0300

    ui: rework `trunk-admin` into `trunk-auth`

--- trunk-auth/src/App.tsx
+++ trunk-auth/src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from "@clerk/clerk-react";
-import { Box, ChakraProvider, StackDivider, VStack } from '@chakra-ui/react'
+import { Box, Center, ChakraProvider, StackDivider, VStack } from '@chakra-ui/react'
 
 if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
 const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 
-
-import './App.css';
-import ExtensionList from './ExtensionList';
+import RequestCard from './RequestCard';
 
 function App() {
   return (
@@ -28,15 +26,17 @@ function App() {
 }
 
 function SignedAdmin() {
-  return <div>
-    <VStack
-      divider={<StackDivider borderColor='gray.200' />}
-      spacing={4}
-      align='stretch'
-    >
-      <ExtensionList />
-    </VStack>
-  </div>;
+  return (
+      <Box 
+        w='100vw' h='100vh'
+        bgGradient='linear(to-r, gray.600, gray.900)'
+        display="flex" alignItems='center' gap='2'
+      >
+        <Center height='100vh' width='100vw'>
+          <RequestCard />
+        </Center>
+      </Box>
+  );
 }
 
 export default App;
