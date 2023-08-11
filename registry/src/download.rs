239195070b33b6228bd9e2106a12ffa8454fcbba
Ian Stanton <ian@coredb.io>
2023-08-11 13:18:21 -0400

    Revert

--- registry/src/download.rs
+++ registry/src/download.rs
@@ -13,7 +13,7 @@ pub async fn latest_version(
         .fetch_one(&mut tx)
         .await?;
     let id: i32 = ext.id as i32;
-    let latest = sqlx::query!("SELECT * FROM versions WHERE extension_id = $1 ORDER BY string_to_array(num, '.')::int[] DESC LIMIT 1;", id)
+    let latest = sqlx::query!("SELECT num FROM versions WHERE extension_id = $1 ORDER BY string_to_array(num, '.')::int[] DESC LIMIT 1;", id)
         .fetch_one(&mut tx)
         .await?;
     Ok(latest.num.unwrap())
