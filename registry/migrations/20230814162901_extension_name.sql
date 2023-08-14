320b7a938ab8f7c5db260a1c0bcb5d440545275f
Ian Stanton <ian@coredb.io>
2023-08-14 16:08:14 -0400

    Update migration comment

--- registry/migrations/20230814162901_extension_name.sql
+++ registry/migrations/20230814162901_extension_name.sql
@@ -1,4 +1,4 @@
--- Add user_name column to api_tokens
+-- Add extension_name column to versions table
 ALTER TABLE versions
 ADD COLUMN extension_name VARCHAR;
 UPDATE versions SET extension_name = e.name FROM extensions e WHERE versions.extension_id = e.id AND versions.extension_name IS NULL;
