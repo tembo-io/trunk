83002a0f04dd429f0957625fc6f2d3f5ffa21cbf
Evan Stanton <evanstanton@Evans-MBP.fritz.box>
2023-08-25 22:33:57 +0200

    Cargo fmt

--- registry/src/routes/extensions.rs
+++ registry/src/routes/extensions.rs
@@ -468,7 +468,6 @@ pub async fn delete_extension(
 pub async fn get_shared_preload_libraries(
     conn: web::Data<Pool<Postgres>>,
 ) -> Result<Json<Vec<String>>, ExtensionRegistryError> {
-
     // Query to get extension names from the appropriate table.
     let rows = sqlx::query!("SELECT name FROM shared_preload_libraries")
         .fetch_all(&mut tx)
