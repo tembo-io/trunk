c2d8b6624246c9c50fa8fb543acffe3cf8aed947
Ry Walker <4283+ryw@users.noreply.github.com>
2023-09-11 20:58:54 -0400

    Optimize elephant logo and delete unused images (#449)

--- ui/src/Components/Footer/index.tsx
+++ ui/src/Components/Footer/index.tsx
@@ -1,13 +1,13 @@
 import styles from "./footer.module.scss";
 import Image from "next/image";
-import TrunkLogo from "/public/TemboElephant.png";
+import TemboLogo from "/public/TemboElephant.png";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <a className={styles.link} href="https://tembo.io" target="_blank">
         <span className={styles.small}> Sponsored by</span>
-        <Image className={styles.image} src={TrunkLogo} width={20} height={20} alt="Tembo logo"></Image>Tembo
+        <Image className={styles.image} src={TemboLogo} width={20} height={20} alt="Tembo logo"></Image>Tembo
       </a>
     </footer>
   );
