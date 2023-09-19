ce563415b96b26a337f6cc260fb19a10ecd6c3e5
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2023-09-19 16:11:33 -0300

    registry: wire `/admin/extensions/libraries/{library}` into server

--- registry/src/server.rs
+++ registry/src/server.rs
@@ -26,7 +26,8 @@ pub fn routes_config(configuration: &mut web::ServiceConfig) {
             web::scope("/admin")
                 .wrap(ClerkMiddleware::new(clerk_cfg, None))
                 .service(routes::root::auth_ok)
-                .service(routes::extensions::delete_extension),
+                .service(routes::extensions::delete_extension)
+                .service(routes::extensions::put_shared_preload_libraries),
         );
 }
 
