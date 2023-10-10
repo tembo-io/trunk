0a163e288ee0eae66767079aa583ae0fbae6dc60
Darren B <68653294+Devd0@users.noreply.github.com>
2023-10-10 16:35:34 -0400

    init env_logger

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -8,9 +8,14 @@ mod trunk_toml;
 use crate::commands::SubCommand;
 use async_trait::async_trait;
 use clap::{Parser, Subcommand};
+use log::Level;
+use log::error;
 
 use std::time::Duration;
 use tokio_task_manager::{Task, TaskManager};
+use env_logger;
+use colorful::{Color, Colorful, RGB};
+use std::io::Write;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -45,6 +50,21 @@ impl SubCommand for SubCommands {
 }
 
 fn main() {
+    env_logger::builder()
+		.filter_level(log::LevelFilter::Info)
+		.format(|buf, record| {
+            let level_str = match record.level() {
+                Level::Info => String::from("info").color(RGB::new(255, 247, 240)),
+                Level::Error => String::from("error").color(Color::Red),
+                Level::Warn => String::from("warn").color(Color::Yellow),
+                Level::Debug => String::from("debug").color(RGB::new(234, 67, 118)),
+                Level::Trace => String::from("trace").color(Color::Green),
+            };
+            return writeln!(buf, "{}: {}", level_str, record.args());
+        })
+		.try_init()
+		.ok();
+
     let tm = TaskManager::new(Duration::from_secs(60));
 
     let rt = tokio::runtime::Builder::new_multi_thread()
@@ -52,11 +72,16 @@ fn main() {
         .build()
         .unwrap();
 
-    rt.block_on(async {
+    match rt.block_on(async {
         let cli = Cli::parse();
         let result = cli.command.execute(tm.task()).await;
         tm.wait().await;
         result
-    })
-    .expect("error occurred");
+    }) {
+        Ok(_) => {} // Do nothing if we succeed (let the command finish)
+        Err(e) => {
+            error!("{}", e);
+            std::process::exit(1);
+        }
+    }
 }
