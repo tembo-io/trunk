07c1b7d3ee84b56df4e93c8695333b418802cffa
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2023-10-12 11:15:58 -0300

    registry: keep track of download counts

--- registry/src/download.rs
+++ registry/src/download.rs
@@ -1,12 +1,11 @@
 //! Functionality for downloading extensions and maintaining download counts
 use crate::errors::ExtensionRegistryError;
-use actix_web::web;
 use sqlx::{Pool, Postgres};
 
 /// Find an extension's lastest version given it's extension_id
 pub async fn latest_version(
     extension_id: i32,
-    conn: web::Data<Pool<Postgres>>,
+    conn: &Pool<Postgres>,
 ) -> Result<String, ExtensionRegistryError> {
     // Create a transaction on the database
     let mut tx = conn.begin().await?;
@@ -14,4 +13,23 @@ pub async fn latest_version(
         .fetch_one(&mut tx)
         .await?;
     Ok(latest.num.unwrap())
+}
+
+pub async fn check_version(
+    pool: &Pool<Postgres>,
+    extension_id: i32,
+    version: &str,
+) -> Result<bool, ExtensionRegistryError> {
+    let record = sqlx::query!(
+        "SELECT COUNT(*) > 0 AS version_exists
+        FROM versions
+        WHERE extension_id = $1
+        AND num = $2",
+        extension_id,
+        version
+    )
+    .fetch_one(pool)
+    .await?;
+
+    Ok(record.version_exists.unwrap_or(false))
 }
--- registry/src/routes/download.rs
+++ registry/src/routes/download.rs
@@ -1,6 +1,6 @@
 //! Functionality for downloading extensions and maintaining download counts
-use crate::download::latest_version;
-use crate::errors::ExtensionRegistryError;
+use crate::download::{check_version, latest_version};
+use crate::errors::Result;
 use crate::uploader::extension_location;
 use crate::{config::Config, extensions::get_extension_id};
 use actix_web::{get, web, HttpResponse};
@@ -14,16 +14,38 @@ pub async fn download(
     conn: web::Data<Pool<Postgres>>,
     cfg: web::Data<Config>,
     path: web::Path<(String, String)>,
-) -> Result<HttpResponse, ExtensionRegistryError> {
+) -> Result<HttpResponse> {
     let (name, mut version) = path.into_inner();
-    // TODO(ianstanton) Increment download count for extension
+    let extension_id = get_extension_id(&name, conn.as_ref()).await?;
+
     // Use latest version if 'latest' provided as version
     if version == "latest" {
-        let extension_id = get_extension_id(&name, conn.as_ref()).await?;
-        version = latest_version(extension_id as _, conn).await?;
+        version = latest_version(extension_id as _, conn.as_ref()).await?;
+    } else {
+        let version_exists = check_version(conn.as_ref(), extension_id as _, &version).await?;
+        if !version_exists {
+            return Ok(HttpResponse::NotFound().body("Version not found"));
+        }
     }
     let url = extension_location(&cfg.bucket_name, &name, &version);
-    info!("Download requested for {} version {}", name, version);
-    info!("URL: {}", url);
+    info!(
+        "Download requested for {} version {}. URL: {}",
+        name, version, url
+    );
+    increase_download_count(conn.as_ref(), extension_id as _).await?;
+
     Ok(HttpResponse::Ok().body(url))
+}
+
+async fn increase_download_count(pool: &Pool<Postgres>, extension_id: i32) -> Result {
+    sqlx::query!(
+        "UPDATE versions
+        SET download_count = download_count + 1
+        WHERE extension_id = $1",
+        extension_id
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(())
 }
