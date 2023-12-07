750483cf76b84551d80daa0bac73e17ec9dcf7ae
Darren B <68653294+Devd0@users.noreply.github.com>
2023-12-07 11:44:25 -0500

    remove github token from nextjs.config.js env

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
